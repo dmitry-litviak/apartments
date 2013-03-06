4c9cef8090fb1f7a42e8fe685efad8b091048ffb
Dmitry <dmitry.litviak@gmail.com>
2013-03-06 20:51:53 +0400

    design changes

--- application/views/apartments/index.php
+++ application/views/apartments/index.php
@@ -5,18 +5,35 @@
     <?php foreach ($apartments as $apartment): ?>
         <div class="media bordered">
             <a class="pull-left" href="#">
-                <img class="media-object" src="<?php echo Helper_Output::get_apartment($apartment, "small_") ?>">
+                <img class="media-object bordered-img" src="<?php echo Helper_Output::get_apartment($apartment, "small_") ?>">
             </a>
             <div class="media-body">
                 <h4 class="media-heading"><?php echo $apartment->title ?></h4>
-                <h5>Description:</h5>
-                <p><?php echo $apartment->descr ?></p>
-                <h5>Type/Number of rooms:</h5><?php echo $apartment->type->title ?>
-                <h5>Address:</h5><?php echo $apartment->address ?>
-                <h5>Cost:</h5><span class="label label-success">$<?php echo $apartment->cost ?></span>
+                <span>
+                    <strong>Description:</strong>
+                    <?php echo $apartment->descr ?>
+                </span>
+                <hr class="hr-thin">
+                <span>
+                    <strong>Type/Number of rooms:</strong>
+                    <?php echo $apartment->type->title ?>
+                </span>
+                <hr class="hr-thin">
+                <span>
+                    <strong>Address:</strong>
+                    <?php echo $apartment->address ?>
+                </span>
+                <hr class="hr-thin">
+                <span>
+                    <strong>Cost:</strong>
+                    <span class="label label-success">$<?php echo $apartment->cost ?></span>
+                </span>
+                <hr class="hr-thin">
             </div>
         </div>
     <?php endforeach; ?>
 <?php endif; ?>
 <br>
+<div class="center">
 <a class="btn btn-large btn-primary" href="<?php echo URL::site('apartments/create') ?>">Create Apartment</a>
+</div>
