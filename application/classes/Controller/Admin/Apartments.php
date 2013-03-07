99548b336de2f4484e9a33fd1741cd4fbbb7198d
Dmitry <dmitry.litviak@gmail.com>
2013-03-07 21:25:53 +0400

    apartments editing

--- application/classes/Controller/Admin/Apartments.php
+++ application/classes/Controller/Admin/Apartments.php
@@ -63,47 +63,43 @@ public function action_create()
     public function action_edit()
     {
         $apartment = ORM::factory('Apartment')->where('id', '=', $this->request->param('id'))->find();
-        if ($this->logged_user->id == $apartment->user_id) {
-            Helper_Mainmenu::setActiveItem('apartments');
-            Helper_Output::factory()->link_css('bootstrap.fileupload.min')
-                                    ->link_css('jquery-ui-1.8.16.custom')
-                                    ->link_js('libs/jquery.validate.min')
-                                    ->link_js('libs/bootstrap.fileupload.min')
-                                    ->link_js('admin/apartments/create');
-            if ($this->request->post()) { 
-                $post = Helper_Output::clean($this->request->post());
-                $post['user_id'] = $this->logged_user->id;
-                $model = ORM::factory('Apartment', $this->request->param('id'));
-                try {
-                    $model->values($post);
+        Helper_Mainmenu::setActiveItem('apartments');
+        Helper_Output::factory()->link_css('bootstrap.fileupload.min')
+                                ->link_css('jquery-ui-1.8.16.custom')
+                                ->link_js('libs/jquery.validate.min')
+                                ->link_js('libs/bootstrap.fileupload.min')
+                                ->link_js('admin/apartments/create');
+        if ($this->request->post()) { 
+            $post = Helper_Output::clean($this->request->post());
+            $post['user_id'] = $this->logged_user->id;
+            $model = ORM::factory('Apartment', $this->request->param('id'));
+            try {
+                $model->values($post);
+                $model->save();
+                if ($_FILES['image']['tmp_name']) {
+                    $place_upload_dir = Kohana::$config->load('config')->get('apartments_files') . $model->id . '/photos/';
+                    if(!is_dir($place_upload_dir))
+                        mkdir($place_upload_dir, 0777, true);
+                    $name   = basename(md5($_FILES['image']['name'].time())). '.' .pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+                    $target = $place_upload_dir . $name;
+                    move_uploaded_file($_FILES['image']['tmp_name'], $target);
+                    $image_small = Image::factory($target)->resize(265, 265);
+                    $image_small->save($place_upload_dir . 'small_' . $name);
+                    $model->img = $name;
                     $model->save();
-                    if ($_FILES['image']['tmp_name']) {
-                        $place_upload_dir = Kohana::$config->load('config')->get('apartments_files') . $model->id . '/photos/';
-                        if(!is_dir($place_upload_dir))
-                            mkdir($place_upload_dir, 0777, true);
-                        $name   = basename(md5($_FILES['image']['name'].time())). '.' .pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
-                        $target = $place_upload_dir . $name;
-                        move_uploaded_file($_FILES['image']['tmp_name'], $target);
-                        $image_small = Image::factory($target)->resize(265, 265);
-                        $image_small->save($place_upload_dir . 'small_' . $name);
-                        $model->img = $name;
-                        $model->save();
-                    }
-                    $this->redirect('admin/apartments');
                 }
-                catch (ORM_Validation_Exception $e) {
-                    Helper_Alert::setStatus('error');
-                    Helper_Alert::set_flash($e->errors('User'));
-                }
+                $this->redirect('admin/apartments');
             }
-            $data['apartment'] = $apartment;
-            $data['types'] = ORM::factory('Type')->find_all();
-            $this->setTitle('Apartments Edit Page')
-                 ->view('admin/apartments/edit', $data)
-                 ->render();
-        } else {
-            $this->redirect('admin/apartments');
+            catch (ORM_Validation_Exception $e) {
+                Helper_Alert::setStatus('error');
+                Helper_Alert::set_flash($e->errors('User'));
+            }
         }
+        $data['apartment'] = $apartment;
+        $data['types'] = ORM::factory('Type')->find_all();
+        $this->setTitle('Apartments Edit Page')
+             ->view('admin/apartments/edit', $data)
+             ->render();
     }
     
     public function action_delete() {
