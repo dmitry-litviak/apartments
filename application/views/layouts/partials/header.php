fe79c495bc8741c72d91d8516d297faafc5cbffe
Dmitry <dmitry.litviak@gmail.com>
2013-04-06 12:45:04 +0400

    new design

--- application/views/layouts/partials/header.php
+++ application/views/layouts/partials/header.php
@@ -1,14 +1,17 @@
-      <div class="navbar navbar-fixed-top">
-        <div class="navbar-inner">
-          <div class="container">
+<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
             <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
             </button>
-            <a class="brand" href="<?php echo URL::base() ?>">Apartments Site</a>
+            <div class="span2">
+                <a class="brand my-brand" href="<?php echo URL::base() ?>">Apartments Site</a><br>
+                <label class="tag-line">Tag Line</label>
+            </div>
             <div class="nav-collapse collapse">
-              <ul class="nav">
+                <ul class="nav">
                     <?php echo Helper_Mainmenu::render() ?>
                     <?php if (Auth::instance()->get_user()): ?>
                         <?php $role = Auth::instance()->get_user()->roles->order_by('role_id', 'desc')->find()->name ?>
@@ -16,8 +19,8 @@
                             <li><a href="<?php echo URL::site('admin/dashboard') ?>">Admin Panel</a></li>
                         <?php endif; ?>
                     <?php endif; ?>
-              </ul>
+                </ul>
             </div><!--/.nav-collapse -->
-          </div>
         </div>
-      </div>
+    </div>
+</div>
