11b35688a2e5b325c86259ccc6e1e0b9e9be3c69
Dmitry <dmitry.litviak@gmail.com>
2013-04-12 21:58:52 +0400

    mail

--- application/views/mail/alert.php
+++ application/views/mail/alert.php
@@ -1,6 +1,6 @@
 <html>
     <body>
         <p>You have new items in one of yours alert. Follow the link to view them.</p>
-        <a href="<?php echo $obj['url'] ?>"><?php $obj['title'] ?></a>
+        <a href="<?php echo $obj['url'] ?>"><?php echo $obj['title'] ?></a>
     </body>
 </html>
